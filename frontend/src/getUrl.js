1582756c5fddb130c74949f24c0749ddc8f4b814
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2021-12-31 14:02:10 +0200

    Add getUrl.js to frontend.

---
+++ frontend/src/getUrl.js
@@ -0,0 +1,9 @@
+// Gets localhost url if when running a development build and the real url when running the production build.
+function getUrl() {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+        return "http://localhost:8080";
+    } else {
+        return "https://ala-lahti-learn-languages.herokuapp.com";
+    }
+}
+export default getUrl;
