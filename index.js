578fe6c2b67cf736055a3f5189bd6d8ade6b4d35
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2021-12-31 11:39:31 +0200

    Add test to get data from the database using mysql.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+require("dotenv").config();
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
@@ -7,3 +9,21 @@ const server = app.listen(port, () => {
 });
 
 app.use(express.static("frontend/build"));
+
+const config = {
+    host: "mydb.tamk.fi",
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+    connectionLimit: 10,
+};
+const pool = mysql.createPool(config);
+app.use("/data", (req, res) => {
+    pool.query("SELECT * FROM word_pairs", (error, results) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.send(results);
+        }
+    });
+});
