6791adab01aa1069a17e7012c2c89842813b4b27
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-05 11:40:14 +0200

    Add single-page application header links to frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import TestComponent from "./TestComponent";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <TestComponent />
-        </div>
+        <BrowserRouter>
+            <header>
+                <span>Learn Languages!</span>
+                <Link to="/learn">Learn</Link>
+                <Link to="/teach">Teach</Link>
+            </header>
+            <div className="Content">
+                <Routes>
+                    <Route path="/" element={<TestComponent />}></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
