b07c08dc138ece63c111769e594a21ef3e7fc68e
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-05 13:07:24 +0200

    Add text input to LearnPage component.

--- frontend/src/LearnPage.js
+++ frontend/src/LearnPage.js
@@ -4,16 +4,25 @@ class LearnPage extends React.Component {
     state = { loading: true, data: undefined };
     async componentDidMount() {
         let data = await fetch(`${getUrl()}/data`);
-        console.log(data);
         let obj = await data.json();
         this.setState({ loading: false, data: obj });
-        console.log(obj);
     }
     render() {
         if (this.state.loading) {
             return <div>Loading...</div>;
         } else {
-            return <div>{this.state.data[0].word_in_language1}</div>;
+            return (
+                <div>
+                    {this.state.data[0].word_in_language1}
+                    <form>
+                        <input
+                            type="text"
+                            placeholder={`Type in ${this.state.data[0].language2}...`}
+                        />
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+            );
         }
     }
 }
