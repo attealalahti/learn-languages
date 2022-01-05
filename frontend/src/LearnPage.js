9c481dbf715128e08e89b69d6fc1a751857110b3
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-05 13:41:04 +0200

    Modify LearnPage to cycle through words on submit.

--- frontend/src/LearnPage.js
+++ frontend/src/LearnPage.js
@@ -1,23 +1,35 @@
 import React from "react";
 import getUrl from "./getUrl";
 class LearnPage extends React.Component {
-    state = { loading: true, data: undefined };
+    state = { loading: true, data: undefined, currentWordIndex: 0 };
     async componentDidMount() {
         let data = await fetch(`${getUrl()}/data`);
         let obj = await data.json();
         this.setState({ loading: false, data: obj });
     }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        let nextIndex = this.state.currentWordIndex + 1;
+        if (nextIndex >= this.state.data.length) {
+            nextIndex = 0;
+        }
+        this.setState({ currentWordIndex: nextIndex });
+    };
     render() {
         if (this.state.loading) {
             return <div>Loading...</div>;
         } else {
             return (
                 <div>
-                    {this.state.data[0].word_in_language1}
-                    <form>
+                    <div>
+                        {this.state.data[this.state.currentWordIndex].word_in_language1}
+                    </div>
+                    <form onSubmit={this.handleSubmit}>
                         <input
                             type="text"
-                            placeholder={`Type in ${this.state.data[0].language2}...`}
+                            placeholder={`Type in ${
+                                this.state.data[this.state.currentWordIndex].language2
+                            }...`}
                         />
                         <input type="submit" value="Submit" />
                     </form>
