8442852030170b90433b51d845fa21fa303ba602
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-06 16:07:30 +0200

    Modify LearnPage to keep track of how many words have been answered.

--- frontend/src/LearnPage.js
+++ frontend/src/LearnPage.js
@@ -1,19 +1,22 @@
 import React from "react";
 import getUrl from "./getUrl";
 class LearnPage extends React.Component {
-    state = { loading: true, data: undefined, currentWordIndex: 0 };
+    state = { loading: true, words: undefined, currentWordIndex: 0, wordsAnswered: 0 };
     async componentDidMount() {
         let data = await fetch(`${getUrl()}/words?from=finnish&to=english`);
         let obj = await data.json();
-        this.setState({ loading: false, data: obj });
+        this.setState({ loading: false, words: obj });
     }
     handleSubmit = (event) => {
         event.preventDefault();
         let nextIndex = this.state.currentWordIndex + 1;
-        if (nextIndex >= this.state.data.length) {
+        if (nextIndex >= this.state.words.length) {
             nextIndex = 0;
         }
-        this.setState({ currentWordIndex: nextIndex });
+        this.setState({
+            currentWordIndex: nextIndex,
+            wordsAnswered: this.state.wordsAnswered + 1,
+        });
     };
     render() {
         if (this.state.loading) {
@@ -22,17 +25,21 @@ class LearnPage extends React.Component {
             return (
                 <div>
                     <div>
-                        {this.state.data[this.state.currentWordIndex].word_in_language1}
+                        {this.state.words[this.state.currentWordIndex].word_in_language1}
                     </div>
                     <form onSubmit={this.handleSubmit}>
                         <input
                             type="text"
                             placeholder={`Type in ${
-                                this.state.data[this.state.currentWordIndex].language2
+                                this.state.words[this.state.currentWordIndex].language2
                             }...`}
                         />
                         <input type="submit" value="Submit" />
                     </form>
+                    <div>
+                        Words answered: {this.state.wordsAnswered}/
+                        {this.state.words.length}
+                    </div>
                 </div>
             );
         }
