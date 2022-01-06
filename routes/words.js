66c09aafe7bb80c2a4d71c13f6bba6c2918870ca
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-06 17:31:11 +0200

    Modify /words to catch internal server errors.

--- routes/words.js
+++ routes/words.js
@@ -3,28 +3,31 @@ const connection = require("../database/crudrepository.js");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    if (req.query.to && req.query.from) {
-        let words = await connection.findFromLanguages(req.query.from, req.query.to);
-        let reorderedWords = [];
-        // Flip languages around if necessary to keep languages "from" and "to" consistent
-        words.forEach((word) => {
-            if (word.language1.toLowerCase() === req.query.from) {
-                reorderedWords.push(word);
-            } else {
-                console.log(word);
-                reorderedWords.push({
-                    ...word,
-                    language1: word.language2,
-                    language2: word.language1,
-                    word_in_language1: word.word_in_language2,
-                    word_in_language2: word.word_in_language1,
-                });
-            }
-        });
-        res.send(reorderedWords);
-    } else {
-        let allWords = await connection.findAll();
-        res.send(allWords);
+    try {
+        if (req.query.to && req.query.from) {
+            let words = await connection.findFromLanguages(req.query.from, req.query.to);
+            let reorderedWords = [];
+            // Flip languages around if necessary to keep languages "from" and "to" consistent
+            words.forEach((word) => {
+                if (word.language1.toLowerCase() === req.query.from) {
+                    reorderedWords.push(word);
+                } else {
+                    reorderedWords.push({
+                        ...word,
+                        language1: word.language2,
+                        language2: word.language1,
+                        word_in_language1: word.word_in_language2,
+                        word_in_language2: word.word_in_language1,
+                    });
+                }
+            });
+            res.send(reorderedWords);
+        } else {
+            let allWords = await connection.findAll();
+            res.send(allWords);
+        }
+    } catch (error) {
+        res.status(500).send(error);
     }
 });
 
