eac348ced9386eb861942e0a9c6169ff5a5ae611
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-08 14:23:43 +0200

    Update POST /words schema to fit updated database.

--- routes/words.js
+++ routes/words.js
@@ -3,16 +3,6 @@ const connection = require("../database/crudrepository.js");
 const router = express.Router();
 const Validator = require("jsonschema").Validator;
 const validator = new Validator();
-const schema = {
-    type: "object",
-    properties: {
-        language1: { type: "string" },
-        language2: { type: "string" },
-        word_in_language1: { type: "string" },
-        word_in_language2: { type: "string" },
-    },
-    required: ["language1", "language2", "word_in_language1", "word_in_language2"],
-};
 
 router.get("/", async (req, res) => {
     try {
@@ -45,6 +35,16 @@ router.get("/", async (req, res) => {
         res.status(500).send(error);
     }
 });
+const schema = {
+    type: "object",
+    properties: {
+        language1_id: { type: "number", min: 0 },
+        language2_id: { type: "number", min: 0 },
+        word_in_language1: { type: "string" },
+        word_in_language2: { type: "string" },
+    },
+    required: ["language1_id", "language2_id", "word_in_language1", "word_in_language2"],
+};
 router.post("/", async (req, res) => {
     const validation = validator.validate(req.body, schema);
     if (validation.errors.length > 0) {
