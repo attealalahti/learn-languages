975c6355ecec5fb0b3859e3e44d7fe9ff15b01bd
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-08 15:22:45 +0200

    Modify LearnPage to add callback to get chosen languages from options.

--- frontend/src/LearnPage.js
+++ frontend/src/LearnPage.js
@@ -2,12 +2,24 @@ import React from "react";
 import AnsweringSection from "./AnsweringSection";
 import LearnOptions from "./LearnOptions";
 class LearnPage extends React.Component {
-    state = { stage: "options" };
+    state = { stage: "options", languageFrom: undefined, languageTo: undefined };
+    moveToAnsweringSection = (languageFrom, languageTo) => {
+        this.setState({
+            stage: "answering",
+            languageFrom: languageFrom,
+            languageTo: languageTo,
+        });
+    };
     render() {
         if (this.state.stage === "options") {
-            return <LearnOptions />;
+            return <LearnOptions continue={this.moveToAnsweringSection} />;
         } else if (this.state.stage === "answering") {
-            return <AnsweringSection />;
+            return (
+                <AnsweringSection
+                    languageFrom={this.state.languageFrom}
+                    languageTo={this.state.languageTo}
+                />
+            );
         }
     }
 }
