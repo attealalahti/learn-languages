e4d7e3a6a0c7b4c6cdf5b2a43f987fd120d8ee16
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-08 14:33:04 +0200

    Modify LearnOptions to fetch all languages from the database.

--- frontend/src/LearnOptions.js
+++ frontend/src/LearnOptions.js
@@ -1,8 +1,37 @@
 import React from "react";
+import getUrl from "./getUrl";
+import axios from "axios";
 
 class LearnOptions extends React.Component {
+    state = { loading: true, error: false, languages: undefined };
+    componentDidMount = async () => {
+        try {
+            let languagesResponse = await axios.get(`${getUrl()}/languages`);
+            this.setState({ loading: false, languages: languagesResponse.data });
+        } catch (error) {
+            this.setState({ loading: false, error: true });
+        }
+    };
     render() {
-        return <div>options</div>;
+        if (this.state.loading) {
+            return <div>Loading...</div>;
+        } else if (this.state.error) {
+            return (
+                <div>
+                    Error
+                    <br />
+                    Failed to load page content
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    {this.state.languages.map((lang) => {
+                        return <div>{lang.language}</div>;
+                    })}
+                </div>
+            );
+        }
     }
 }
 
