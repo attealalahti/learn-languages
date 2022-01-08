f421e651d069f62c51d198d6859db87c8a8a2818
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-08 16:14:58 +0200

    Modify EditWordPairs to fetch word pairs from the database.

--- frontend/src/EditWordPairs.js
+++ frontend/src/EditWordPairs.js
@@ -1,9 +1,35 @@
 import React from "react";
+import axios from "axios";
+import getUrl from "./getUrl";
 
 class EditWordPairs extends React.Component {
+    state = { loading: true, error: false, words: undefined };
+    async componentDidMount() {
+        try {
+            let wordsResponse = await axios.get(
+                `${getUrl()}/words?from=${this.props.language1}&to=${
+                    this.props.language2
+                }`
+            );
+            this.setState({ loading: false, words: wordsResponse.data });
+        } catch (error) {
+            this.setState({ loading: false, error: true });
+        }
+    }
     render() {
-        return <div>Edit word pairs</div>;
+        if (this.state.loading) {
+            return <div>Loading...</div>;
+        } else if (this.state.error) {
+            return (
+                <div>
+                    Error
+                    <br />
+                    Failed to load page content
+                </div>
+            );
+        } else {
+            return <div>Edit word pairs</div>;
+        }
     }
 }
-
 export default EditWordPairs;
