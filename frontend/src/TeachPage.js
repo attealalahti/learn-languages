da69427fc777059b5a17f5331f4579b8220be076
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-08 16:07:03 +0200

    Modify TeachPage to switch between TeachOptions and EditWordPairs.

--- frontend/src/TeachPage.js
+++ frontend/src/TeachPage.js
@@ -1,23 +1,23 @@
 import React from "react";
-import AnsweringSection from "./AnsweringSection";
-import LearnOptions from "./LearnOptions";
+import EditWordPairs from "./EditWordPairs";
+import TeachOptions from "./TeachOptions";
 class TeachPage extends React.Component {
-    state = { stage: "options", languageFrom: undefined, languageTo: undefined };
-    moveToAnsweringSection = (languageFrom, languageTo) => {
+    state = { stage: "options", language1: undefined, language2: undefined };
+    moveToEditing = (language1, language2) => {
         this.setState({
-            stage: "answering",
-            languageFrom: languageFrom,
-            languageTo: languageTo,
+            stage: "editing",
+            language1: language1,
+            language2: language2,
         });
     };
     render() {
         if (this.state.stage === "options") {
-            return <LearnOptions continue={this.moveToAnsweringSection} />;
-        } else if (this.state.stage === "answering") {
+            return <TeachOptions continue={this.moveToEditing} />;
+        } else if (this.state.stage === "editing") {
             return (
-                <AnsweringSection
-                    languageFrom={this.state.languageFrom}
-                    languageTo={this.state.languageTo}
+                <EditWordPairs
+                    language1={this.state.language1}
+                    language2={this.state.language2}
                 />
             );
         }
