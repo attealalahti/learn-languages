ad9bdf7d97b95b16e179557032e27c6ee1c8b7e2
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-08 14:05:46 +0200

    Add route /languages.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const words = require("./routes/words.js");
+const languages = require("./routes/languages.js");
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
@@ -16,3 +17,4 @@ app.use(express.static("frontend/build"));
 
 app.use(express.json());
 app.use("/words", words);
+app.use("/languages", languages);
---
+++ routes/languages.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const connection = require("../database/crudrepository.js");
+const router = express.Router();
+
+module.exports = router;
