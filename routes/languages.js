0273917dbbd10889c6c5291b333cdcb5d18d0a41
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-08 14:29:42 +0200

    Add GET /languages to RESTful api.

--- routes/languages.js
+++ routes/languages.js
@@ -2,4 +2,13 @@ const express = require("express");
 const connection = require("../database/crudrepository.js");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+    try {
+        let allLanguages = await connection.findAllLanguages();
+        res.send(allLanguages);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 module.exports = router;
