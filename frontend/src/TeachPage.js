4e6bc5cb8a76f282a0e21c2c50fd7a4cf2d57c77
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-10 18:47:30 +0200

    Modify TeachPage to create a menu between languages and words.

--- frontend/src/TeachPage.js
+++ frontend/src/TeachPage.js
@@ -1,8 +1,26 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 class TeachPage extends React.Component {
     render() {
-        return <div>test</div>;
+        return (
+            <Card>
+                <Card.Body>
+                    <Link to="/teach/languages">
+                        <Button className="LinkButton" size="lg">
+                            Edit languages
+                        </Button>
+                    </Link>
+                    <Link to="/teach/words">
+                        <Button className="LinkButton" size="lg">
+                            Edit words
+                        </Button>
+                    </Link>
+                </Card.Body>
+            </Card>
+        );
     }
 }
 
