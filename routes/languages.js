48775cabf827cf9908224c1304ca141ba8c89110
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-10 19:17:10 +0200

    Add POST /languages to RESTful api.

--- routes/languages.js
+++ routes/languages.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const connection = require("../database/crudrepository.js");
 const router = express.Router();
+const Validator = require("jsonschema").Validator;
+const validator = new Validator();
+const schema = {
+    type: "object",
+    properties: { language: { type: "string" } },
+    required: ["language"],
+};
 
 router.get("/", async (req, res) => {
     try {
@@ -8,6 +15,19 @@ router.get("/", async (req, res) => {
         res.send(allLanguages);
     } catch (error) {
         res.status(500).send(error);
+    }
+});
+router.post("/", async (req, res) => {
+    const validation = validator.validate(req.body, schema);
+    if (validation.errors.length > 0) {
+        res.status(400).send(validation.errors);
+    } else {
+        try {
+            let id = await connection.saveLanguage(req.body.language);
+            res.status(201).send({ ...req.body, id: id });
+        } catch (error) {
+            res.status(500).send(error);
+        }
     }
 });
 
