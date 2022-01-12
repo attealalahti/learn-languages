2924896b8be3565854dd2ac3a4ee5ebd7b9b5625
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-12 19:56:31 +0200

    Document index.js.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+/**
+ * Serves [/words]{@link module:routes/words}, [/languages]{@link module:routes/languages} and the frontend.
+ * @author Atte Ala-Lahti
+ */
 const app = express();
 const cors = require("cors");
 const words = require("./routes/words.js");
@@ -14,4 +18,5 @@ app.use(express.json());
 app.use("/words", words);
 app.use("/languages", languages);
 app.use(express.static("frontend/build"));
+// Route all other urls also to the frontend
 app.use("*", express.static("frontend/build"));
