4c7a576a68eb66d884d85f6fc825cd7d1af2b6f2
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-14 01:49:30 +0200

    Document EditableLanguage component class.

--- frontend/src/EditableLanguage.js
+++ frontend/src/EditableLanguage.js
@@ -3,6 +3,31 @@ import EditableContent from "./EditableContent";
 import axios from "axios";
 import getUrl from "./getUrl";
 
+/**
+ * Callback called before trying to connect to the database.
+ * @callback startConnectCallback
+ */
+
+/**
+ * Callback called after database connection ends.
+ * @callback stopConnectCallback
+ */
+
+/**
+ * Callback called when a database connection error is caught.
+ * @callback errorCallback
+ */
+
+/**
+ * A component that renders and passes all props to a {@link EditableContent} component and
+ * contains the {@link saveChangesCallback} for it to save changes to a language.
+ * @property {number} props.id - The id of the language to be patched.
+ * @property {startConnectCallback} props.startConnect - Callback called before trying to connect to the database.
+ * @property {stopConnectCallback} props.stopConnect - Callback called after database connection ends.
+ * @property {errorCallback} props.error - Callback called when a database connection error is caught.
+ * @author Atte Ala-Lahti
+ * @extends React.Component
+ */
 class EditableLanguage extends React.Component {
     saveLanguage = async (newContent) => {
         this.props.startConnect();
