f9b5f3e6089b14ba3b07cdd93059d4aa9a2e9935
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-13 20:42:22 +0200

    Modify App to be a class component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 import LearnPage from "./LearnPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TeachPage from "./TeachPage";
@@ -10,33 +11,38 @@ import NavBar from "react-bootstrap/NavBar";
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
 
-function App() {
-    return (
-        <BrowserRouter>
-            <NavBar bg="primary" variant="dark">
-                <LinkContainer to="/">
-                    <NavBar.Brand> Learn Languages!</NavBar.Brand>
-                </LinkContainer>
-                <Nav>
-                    <LinkContainer to="/learn">
-                        <Nav.Link>Learn</Nav.Link>
+class App extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <NavBar bg="primary" variant="dark">
+                    <LinkContainer to="/">
+                        <NavBar.Brand> Learn Languages!</NavBar.Brand>
                     </LinkContainer>
-                    <LinkContainer to="/teach">
-                        <Nav.Link>Teach</Nav.Link>
-                    </LinkContainer>
-                </Nav>
-            </NavBar>
-            <div className="Content">
-                <Routes>
-                    <Route path="/" element={<HomePage />}></Route>
-                    <Route path="/learn" element={<LearnPage />}></Route>
-                    <Route path="/teach" element={<TeachPage />}></Route>
-                    <Route path="/teach/words" element={<TeachWordsPage />}></Route>
-                    <Route path="/teach/languages" element={<EditLanguages />}></Route>
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+                    <Nav>
+                        <LinkContainer to="/learn">
+                            <Nav.Link>Learn</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/teach">
+                            <Nav.Link>Teach</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </NavBar>
+                <div className="Content">
+                    <Routes>
+                        <Route path="/" element={<HomePage />}></Route>
+                        <Route path="/learn" element={<LearnPage />}></Route>
+                        <Route path="/teach" element={<TeachPage />}></Route>
+                        <Route path="/teach/words" element={<TeachWordsPage />}></Route>
+                        <Route
+                            path="/teach/languages"
+                            element={<EditLanguages />}
+                        ></Route>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
