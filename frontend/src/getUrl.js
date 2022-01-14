168f190ee23d629cb8b2d1c0cd36bb4cde0b5108
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-14 02:30:20 +0200

    Document getUrl.js.

--- frontend/src/getUrl.js
+++ frontend/src/getUrl.js
@@ -1,4 +1,17 @@
 // Gets localhost url when running a development build and the real url when running the production build.
+
+/**
+ * A function for choosing the correct RESTful api url.
+ * @module
+ * @author Atte Ala-Lahti
+ */
+
+/**
+ * Returns the url for RESTful api.
+ * Return a localhost url for testing when running a development build.
+ * Returns the real Heroku url when running the production build.
+ * @returns {string} A localhost url or the Heroku url.
+ */
 function getUrl() {
     if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
         return "http://localhost:8080";
