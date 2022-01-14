09a00fc547b1f3d79f3b3c6912990e00cb44802c
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-14 13:22:03 +0200

    Document component TeachPage.

--- frontend/src/TeachPage.js
+++ frontend/src/TeachPage.js
@@ -3,7 +3,16 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { LinkContainer } from "react-router-bootstrap";
 
+/**
+ * A component that renders the main Teach page.
+ * Contains links to editing languages and editing words.
+ */
 class TeachPage extends React.Component {
+    /**
+     * Renders the Teach page containing two buttons that link to
+     * /teach/languages and /teach/words.
+     * @returns {React.Component} The Teach page.
+     */
     render() {
         return (
             <Card>
