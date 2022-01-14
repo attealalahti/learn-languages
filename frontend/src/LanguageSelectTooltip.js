fa6770aab8278ed6b5d2b824031c756289742ebb
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-01-14 13:17:15 +0200

    Document component LangugeSelectTooltip.

--- frontend/src/LanguageSelectTooltip.js
+++ frontend/src/LanguageSelectTooltip.js
@@ -2,7 +2,20 @@ import React from "react";
 import Overlay from "react-bootstrap/esm/Overlay";
 import Tooltip from "react-bootstrap/esm/Tooltip";
 
+/**
+ * A component that renders a tooltip on a specified element
+ * to prompt the user to select two different languages.
+ * @property {string} props.targetId - Id of the element this tooltip is supposed to be on.
+ * @property {boolean} props.show - Whether or not to show the tooltip.
+ * @author Atte Ala-Lahti
+ * @extends React.Component
+ */
 class LanguageSelectTooltip extends React.Component {
+    /**
+     * Renders a tooltip on a specified element.
+     * The tooltip prompts the user to select two different languages.
+     * @returns {Overlay} An overlay with a tooltip component.
+     */
     render() {
         return (
             <Overlay
